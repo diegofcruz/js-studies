74a23bf3d8510d17ca8cafb9ff52e305424fc962
Diego Cruz <dfcruz@Diegos-MacBook-Pro.local>
2026-04-17 09:42:22 -0300

    feat: test2 with promise

--- nodejs/test2.ts
+++ nodejs/test2.ts
@@ -11,3 +11,39 @@
 // 5. Add a single `.catch()` at the end of the chain.
 
 
+function getOrderId(): Promise<number> {
+	return new Promise((resolve, reject) => setTimeout(() => resolve(40), 300))
+}
+
+function getOrderDetails(orderId: number): Promise<{ id, product }> {
+	return new Promise((resolve, reject) => setTimeout(() => resolve({ id: 40, product: 'Enlatados' }), 300))
+}
+
+function getShipping(product: string): Promise<string> {
+	return new Promise((resolve, reject) => setTimeout(() => resolve(`${product} will be shipped in two days.`), 300))
+}
+
+const crash: Error = () => { throw new Error("Do not") }
+
+getOrderId()
+	.then((orderId) => {
+		console.log('Order id', orderId)
+		return getOrderDetails(orderId)
+	})
+	.then((order) => {
+		console.log('Order details', order)
+		return getShipping(order.product)
+	})
+	.then((shipping) => {
+		console.log(shipping)
+		return crash()
+	})
+	.then(() => {
+		console.log(`We shouldn't see it`)
+	})
+	.catch((err) => console.log('Some error ', err))
+
+// What to observe
+// We have a chain of the then(), one starts when the other finish.
+// The outputs of the previus then(), is the input in the next.
+// When something crash, they jump to the crash event
